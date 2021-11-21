6d37f209e98f961c8a41ed0bff573c051f62c7c3
mqifjjfwiot <85283021+mqifjjfwiot@users.noreply.github.com>
2021-11-22 00:03:12 +0900

    Update recommend_by_genre.py

--- recommend_by_genre.py
+++ recommend_by_genre.py
@@ -1,19 +1,3 @@
-# -*- coding: utf-8 -*-
-# ---
-# jupyter:
-#   jupytext:
-#     formats: ipynb,py:light
-#     text_representation:
-#       extension: .py
-#       format_name: light
-#       format_version: '1.5'
-#       jupytext_version: 1.13.1
-#   kernelspec:
-#     display_name: Python 3
-#     language: python
-#     name: python3
-# ---
-
 import pandas as pd 
 import numpy as np
 from ast import literal_eval
